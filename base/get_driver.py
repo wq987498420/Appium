504f8fb804e1bb253eb1a263f726c4951452c66f
王其 <123123132@qq.com>
2018-10-21 15:33:36 +0800

    注释

--- base/get_driver.py
+++ base/get_driver.py
@@ -8,4 +8,4 @@ def get_driver():
     dict_1['appPackage'] = "com.yunmall.lc"
     dict_1['appActivity'] = "com.yunmall.ymctoc.ui.activity.MainActivity"
     dict_1['automationName'] = 'Uiautomator2'
-    return webdriver.Remote('http://192.168.45.35:4723/wd/hub', dict_1)
+    return webdriver.Remote('http://192.168.45.23:4723/wd/hub', dict_1)
